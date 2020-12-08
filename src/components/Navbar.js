fccaf46c959438afb1260bf9a2e2bf74c8159ce9
patrykjachimowski <p.jachimowski89@gmail.com>
2020-12-08 18:50:24 +0100

    navbar construction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,89 @@
-import React from 'react';
-import { AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box } from "@material-ui/core";
-import { ArrowBack, AssigmentInd, Home, Apps, ContactMail } from "@material-ui/icons";
-import avatar from "../avatar.png";
+import React, { useState } from 'react';
+import { AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box, ListItemIcon } from '@material-ui/core';
+import { ArrowBack, AssignmentInd, Home, Apps, ContactMail } from '@material-ui/icons';
+import MobileRightMenuSlider from "@material-ui/core/Drawer";
+import { makeStyles } from '@material-ui/core/styles';
+import avatar from '../avatar.png';
+
+// CSS STYLES 
+
+const useStyles = makeStyles(theme => ({
+    menuSliderContainer: {
+        width: 250,
+        background: "#511",
+        height: "100%",
+    },
+    avatar: {
+        display: "block",
+        margin: "0.5rem auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13),
+    },
+    listItem: {
+        color: "tan"
+    }
+}));
+
+const menuItems = [
+    {
+        listIcon: <Home />,
+        listText: "Home"
+    },
+    {
+        listIcon: <AssignmentInd />,
+        listText: "Resume"
+    },
+    {
+        listIcon: <Apps />,
+        listText: "Portfolio"
+    },
+    {
+        listIcon: <ContactMail />,
+        listText: "Contacts"
+    }
+]
 
 const Navbar = () => {
+    const [state, setState] = useState({
+        right: false
+    })
+
+    const toggleSlider = (slider, open) => () => {
+        setState({ ...state, [slider]: open })
+    }
+    const classes = useStyles();
+
+    const sideList = slider => (
+        <Box className={classes.menuSliderContainer} component="div">
+            <Avatar className={classes.avatar} src={avatar} alt="Russel Crowe"></Avatar>
+            <Divider />
+            <List>
+                {menuItems.map((lsItem, key) => (
+                    <ListItem button key={key}>
+                        <ListItemIcon className={classes.listItem}>
+                            {lsItem.listIcon}
+                        </ListItemIcon>
+                        <ListItemText className={classes.listItem} primary={lsItem.listText} />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
     return (
         <>
-        <Box component="div">
-            <Avatar src={avatar} alt="Russel Crowe"></Avatar>
-
-        </Box>
             <Box component="nav">
                 <AppBar position="static" style={{ background: "#222" }}>
                     <Toolbar>
-                        <IconButton>
+                        <IconButton onClick={toggleSlider("right", true)}> 
                             <ArrowBack style={{ color: "tomato" }} />
                         </IconButton>
                         <Typography variant="h5" style={{ color: "tan" }}>
                             Portfolio
                     </Typography>
+                        <MobileRightMenuSlider open={state.right} onClose={toggleSlider("right", false)} anchor="right">
+                            {sideList("right")}
+                        </MobileRightMenuSlider>
                     </Toolbar>
                 </AppBar>
             </Box>
